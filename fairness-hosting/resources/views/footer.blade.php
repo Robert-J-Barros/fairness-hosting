715b8f68a538cb5ebb2ee2827f0e9aa00531aaad
Lucas Paiva <lucas.paiva@tradeupgroup.com>
2023-08-07 16:36:53 -0300

    ModifyStructure-Laravel
    Signed-off-by: Lucas Paiva <lucas.paiva@tradeupgroup.com>

---
+++ fairness-hosting/resources/views/footer.blade.php
@@ -0,0 +1,69 @@
+<footer class=" container-fluid bg-dark-color BackGroupFooter" id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-12" id="footer-top">
+                <div class="row justify-content-between">
+                    <div class="col-4">
+                        <h2>Fairness</h2>
+                    </div>
+                    <div class="col-4" id="social-icons">
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-instagram"></i>
+                        <i class="bi bi-twitter"></i>
+                        <i class="bi bi-youtube"></i>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12" id="footer-datails">
+                <div class="row">
+                    <div class="col-12 col-md-4" id="news-container">
+                        <h4>Fique por dentro das novidades</h4>
+                        <p class="secondary-color">Inscreva-ser para saber em primeira mão</p>
+                        <form>
+                            <div class="mb-3">
+                                <input type="email" class="form-control" placeholder="Digite o seu email">
+
+                            </div>
+                            <button class="btn btn-dark">Inscrever</button>
+                        </form>
+                    </div>
+                    <div class="col-12 col-md-4" id="contact-container">
+                        <h4>Formas de contato</h4>
+                        <p class="secondary-color"> (11)9999-9999</p>
+                        <p class="secondary-color">contato@fairness.tech</p>
+                    </div>
+                    <div class="col-12 col-md-4" id="links-container">
+                        <div class="row">
+                            <h4>Voce poder estar procurando por</h4>
+                            <div class="col-6">
+                                <ul class="list-unstyled">
+                                    <li><a href="#" class="secondary-color">Cursos</a></li>
+                                    <li><a href="#" class="secondary-color">Treinamentos</a></li>
+                                    <li><a href="#" class="secondary-color">Suporte</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-6">
+                                <ul class="list-unstyled">
+                                    <li><a href="#" class="secondary-color">Planos</a></li>
+                                    <li><a href="#" class="secondary-color">Trabalhos</a></li>
+                                    <li><a href="#" class="secondary-color">Contato</a></li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12" id="footer-bottom">
+                <div class="row justify-content-between">
+                    <div class="col-12 col-md-3">
+                        <p class="secondary-color">Fairness &copy; 2023</p>
+                    </div>
+                    <div class="col-12 col-md-3">
+                        <p class="secondary-color">Realizamos suas necessidades <i class="bi bi-heart"></i></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ fairness-hosting/resources/views/topMenu.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <section class="container">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-haze2 logo FontsForProject" viewBox="0 0 16 16">
+            <path d="M8.5 3a4.002 4.002 0 0 0-3.8 2.745.5.5 0 1 1-.949-.313 5.002 5.002 0 0 1 9.654.595A3 3 0 0 1 13 12H4.5a.5.5 0 0 1 0-1H13a2 2 0 0 0 .001-4h-.026a.5.5 0 0 1-.5-.445A4 4 0 0 0 8.5 3zM0 7.5A.5.5 0 0 1 .5 7h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm2 2a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-2 4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+        <a class="navbar-brand" href="#">Fairness</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse PositionNavBar" id="navbarNav">
+            <ul class="navbar-nav ">
+            <li class="nav-item active ">
+                <a class="nav-link PositionLinkNavBar" href="#">Wordpress <span class="sr-only"></span></a>
+                </li>
+            <li class="nav-item active ">
+                <a class="nav-link PositionLinkNavBar FontsForProject" href="#">Website Builder <span class="sr-only"></span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link PositionLinkNavBar FontsForProject" href="#">Hosting</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link PositionLinkNavBar FontsForProject" href="#">Dominios</a>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="btn btn-outline-dark FontsForProject">Login</button>
+                </li>
+            </ul>
+        </div>
+    </section>
+</nav>
