27ae3a128ffdc8394433140805dfec6a26dee734
Lucas Paiva <lucas.paiva@tradeupgroup.com>
2023-08-07 13:38:28 -0300

    Trade-StructureLaravel
    Signed-off-by: Lucas Paiva <lucas.paiva@tradeupgroup.com>

--- home/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-def home(request):
-    response = "robert"
-    return HttpResponse(response)
-
-def home(request):
-    return render(request, "home/index.html")
